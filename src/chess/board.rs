92e0c6c3462efb483027f78cdc1dc36204b0254a
robo-monk <rrobomonk@gmail.com>
2022-10-04 02:08:30 +0300

    fen parsing

--- src/chess/board.rs
+++ src/chess/board.rs
@@ -54,12 +54,12 @@ impl Piece {
   }
 }
 
-
 impl Board {
   pub fn new() -> Self {
     Board {
       turn: Color::White,
-      squares: [Some(Piece::new(P::Pawn, Color::White)); 64]
+      // squares: [Some(Piece::new(P::Pawn, Color::White)); 64]
+      squares: [None; 64]
     }
   }
 
@@ -67,15 +67,64 @@ impl Board {
     println!("{}", self.render_acii());
   }
 
+  // rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+  pub fn from_fen(fen: String) -> Self {
+    let mut board = Board::new();
+    // let ranks : String = fen.split(" ").collect();
+    let fields: Vec<&str> = fen.split(" ").collect();
+    // let fields: [&str; 8] = fen.split(" ").collect().try_into();
+
+    let piece_placement_data = fields[0];
+    let active_color = fields[1];
+    let castling = fields[2];
+    let en_passant = fields[3];
+    let halfmoves = fields[4]; // Halfmove clock: The number of halfmoves since the last capture or pawn advance, used for the fifty-move rule.[9]
+    let move_count = fields[5];
+
+    // parse placement data
+    let mut index: usize = 0;
+    piece_placement_data.split("/").for_each(|rank| {
+      rank.chars().for_each(|p| {
+        if p.is_ascii_digit() {
+          print!("ascii digit incr from {}", index);
+          index += p.to_digit(10).unwrap_or_else(|| panic!("expected number")) as usize;
+          println!(" to {}", index);
+        } else {
+          let color =  if p.is_lowercase() { Color::Black } else { Color::White };
+          let class: P = match p.to_lowercase().to_string().as_str() {
+            "p" => P::Pawn,
+            "k" => P::King,
+            "q" => P::Queen,
+            "b" => P::Bishop,
+            "n" => P::Knight,
+            "r" => P::Rook,
+            _ => panic!("unrecognised char")
+          };
+
+          board.squares[index] = Some(Piece::new(class, color));
+          println!("laying to i {}: color: {:?} cls: {:?}", index, color, class);
+          index += 1;
+        }
+      })
+
+
+    });
+
+    board
+  }
   pub fn render_acii(&self) -> String {
     let mut out = String::from("");
-    for row in 0..8 {
+    for rank in 0..8 {
       out += "   ";
       out += &"⎯⎯⎯⎯".repeat(8);
       out += "\n";
-      out += &format!("{}  ", 8-row);
-      for col in 0..8 {
-        out += &format!("⏐ {} ", self.squares[col].unwrap().symbol());
+      out += &format!("{}  ", 8 - rank);
+      for file in 0..8 {
+        if self.squares[file].is_some() {
+          out += &format!("⏐ {} ", self.squares[file].unwrap().symbol());
+        } else {
+          out += &format!("⏐ {} ", " ");
+        }
       }
       out += "⏐\n";
     }
@@ -91,4 +140,6 @@ impl Board {
     // self.squares.iter().for_each(|square| {
     // });
   }
+
+  
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod chess;
 use chess::board::{Board};
 
 fn main() {
-    let board = Board::new();
+    // let board = Board::new();
+    let fen = String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    let board = Board::from_fen(fen);
     dbg!(&board);
+    
     board.print()
 }
