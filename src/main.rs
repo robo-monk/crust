de331261aa8d7545e0ce307123ce1540558e829f
robo-monk <rrobomonk@gmail.com>
2022-10-04 02:57:21 +0300

    nice it works

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ fn main() {
     // let board = Board::new();
     let fen = String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     let mut board = Board::from_fen(&fen);
-    dbg!(&board);
 
     // board.get_square();
     board.make_move("e2", "e4");
+    board.print();
+    board.make_move("d7", "d5");
+    board.print();
+    board.make_move("e4", "d5");
+    board.print();
+    board.make_move("d8", "d5");
     board.print();
 
     // let a = board.get_square(&"E2".to_string());
