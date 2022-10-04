8fa53d7bbf9333ca3ff74ecd22eef69837bfd619
robo-monk <rrobomonk@gmail.com>
2022-10-04 11:10:23 +0300

    various things i dont rememebr

--- src/chess/board.rs
+++ src/chess/board.rs
@@ -1,66 +1,26 @@
-#[derive(Debug, Clone, Copy)]
-pub enum P {
-  Pawn,
-  Knight,
-  Bishop,
-  Rook,
-  Queen,
-  King
-}
+use super::piece::{P, Piece, Color, Direction};
 
-#[derive(Debug, Clone, Copy)]
-pub struct Piece {
-  class: P,
-  color: Color
-}
-
-
-#[derive(Debug, Clone, Copy)]
-pub enum Color {
-  Black,
-  White
+pub struct Move {
+  from: usize,
+  to: usize
 }
 
 #[derive(Debug)]
 pub struct Board {
   turn: Color,
-  squares: [Option<Piece>; 64]
+  move_count: u64,
+  squares: [Option<Piece>; 64],
+  en_passant: Option<usize>,
 }
-
-
-impl Piece {
-  pub fn new(class: P, color: Color) -> Self {
-    Piece {
-      class, color
-    }
-  }
-
-  pub fn symbol(&self) -> &str {
-    match (self.color, self.class) {
-       (Color::White, P::Pawn) => "♙",
-       (Color::White, P::Knight) => "♘",
-       (Color::White, P::Bishop) => "♗",
-       (Color::White, P::Rook) => "♖",
-       (Color::White, P::Queen) => "♕",
-       (Color::White, P::King) => "♔",
-
-       (Color::Black, P::Pawn) => "♟",
-       (Color::Black, P::Knight) => "♞",
-       (Color::Black, P::Bishop) => "♝",
-       (Color::Black, P::Rook) => "♜",
-       (Color::Black, P::Queen) => "♛",
-       (Color::Black, P::King) => "♚",
-    }
-  }
-}
-
 
 impl Board {
   pub fn new() -> Self {
     Board {
       turn: Color::White,
+      move_count: 0,
       // squares: [Some(Piece::new(P::Pawn, Color::White)); 64]
-      squares: [None; 64]
+      squares: [None; 64],
+      en_passant: None,
     }
   }
 
@@ -68,20 +28,22 @@ impl Board {
     println!("{}", self.render_acii());
   }
 
-  pub fn parse_notation(&self, notation: &String) -> usize {
+  pub fn parse_notation(notation: &String) -> Option<usize> {
+    if notation == &String::from("-") { return None }
+
     let chs: Vec<char> = notation.chars().collect();
     let file = chs[0].to_ascii_lowercase() as u32 - 97; // a is 97 in ascii
     let rank = chs[1].to_digit(10).unwrap_or_else(|| panic!("invalid square")); // 1-8
-    ((8-rank)*8 + file) as usize
+   Some(((8-rank)*8 + file) as usize)
   }
 
   pub fn get_square(&self, square: &String) -> Option<Piece> {
-    let i = self.parse_notation(square);
+    let i = Board::parse_notation(square).unwrap();
     self.squares[i]
   }
 
   pub fn set_square(&mut self, square: &String, p: Option<Piece>) -> () {
-    let i = self.parse_notation(square);
+    let i = Board::parse_notation(square).unwrap();
     self.squares[i] = p;
   }
 
@@ -94,24 +56,65 @@ impl Board {
 
     self.set_square(to, piece);
     self.set_square(from, None);
+
+    self.turn = if self.turn == Color::Black {
+      Color::White
+    } else {
+      Color::Black
+    };
+
+    self.move_count += 1;
   }
+
   pub fn make_move(&mut self, from: &str, to: &str) -> () {
     self._make_move(&from.to_string(), &to.to_string());
   }
 
+  pub fn get_available_moves(&self, notation: &str) -> Vec<Move> {
+    let piece = self.get_square(&notation.to_string()).unwrap_or_else(|| panic!("can't get moves of not a piece"));
+
+    if piece.color != self.turn {
+      return vec![];
+    }
+
+    match piece.class {
+      P::Pawn => {
+        todo!()
+      },
+      P::Knight => {
+        // Up + UpLeft
+        // Up + UpRight
+        Direction::Up + Direction::UpLeft;
+        todo!()
+      }
+      _ => panic!("invalid piece?")
+    }
+  }
+
   // rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
   pub fn from_fen(fen: &String) -> Self {
-    let mut board = Board::new();
     // let ranks : String = fen.split(" ").collect();
     let fields: Vec<&str> = fen.split(" ").collect();
     // let fields: [&str; 8] = fen.split(" ").collect().try_into();
 
     let piece_placement_data = fields[0];
-    let active_color = fields[1];
+    let active_color = fields[1]; // w or b
     let castling = fields[2];
     let en_passant = fields[3];
     let halfmoves = fields[4]; // Halfmove clock: The number of halfmoves since the last capture or pawn advance, used for the fifty-move rule.[9]
     let move_count = fields[5];
+
+
+    let mut board = Board {
+      turn:  match active_color {
+        "w" => Color::White,
+        "b" => Color::Black,
+        _ => panic!("invalid fen")
+      },
+      move_count: move_count.parse::<u64>().unwrap(),
+      squares: [None; 64],
+      en_passant: Board::parse_notation(&en_passant.to_string())
+    };
 
     // parse placement data
     let mut index: usize = 0;
@@ -130,7 +133,7 @@ impl Board {
             "b" => P::Bishop,
             "n" => P::Knight,
             "r" => P::Rook,
-            _ => panic!("unrecognised char")
+            _ => panic!("invalid fen")
           };
 
           board.squares[index] = Some(Piece::new(class, color));
@@ -138,12 +141,13 @@ impl Board {
           index += 1;
         }
       })
-
-
     });
 
     board
   }
+
+
+  // cancer
   pub fn render_acii(&self) -> String {
     let mut out = String::from("");
     for rank in 0..8 {
---
+++ src/chess/piece.rs
@@ -0,0 +1,81 @@
+use std::ops::Add;
+
+impl Add for Direction {
+  type Output = i64;
+  fn add(self, rhs: Self) -> Self::Output {
+    self.value() + rhs.value()
+  }
+}
+
+impl Direction {
+  pub fn value(&self) -> i64 {
+    match self {
+      Direction::Up => -8,
+      Direction::Down => 8,
+      Direction::Left => -1,
+      Direction::Right => 1,
+      Direction::UpLeft => Direction::Up + Direction::Left,
+      Direction::UpRight => Direction::Up + Direction::Right,
+      Direction::DownLeft => Direction::Down + Direction::Left,
+      Direction::DownRight => Direction::Down + Direction::Right,
+      _ => panic!("invalid direction")
+    }
+  }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+  Up, Down, // and in the end its only round and round
+  Left, Right, // and round
+  UpLeft, UpRight,
+  DownLeft, DownRight,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Color {
+  Black,
+  White
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum P {
+  Pawn,
+  Knight,
+  Bishop,
+  Rook,
+  Queen,
+  King
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Piece {
+  pub class: P,
+  pub color: Color
+}
+
+impl Piece {
+  pub fn new(class: P, color: Color) -> Self {
+    Piece {
+      class, color
+    }
+  }
+
+  pub fn symbol(&self) -> &str {
+    match (self.color, self.class) {
+       (Color::White, P::Pawn) => "♙",
+       (Color::White, P::Knight) => "♘",
+       (Color::White, P::Bishop) => "♗",
+       (Color::White, P::Rook) => "♖",
+       (Color::White, P::Queen) => "♕",
+       (Color::White, P::King) => "♔",
+
+       (Color::Black, P::Pawn) => "♟",
+       (Color::Black, P::Knight) => "♞",
+       (Color::Black, P::Bishop) => "♝",
+       (Color::Black, P::Rook) => "♜",
+       (Color::Black, P::Queen) => "♛",
+       (Color::Black, P::King) => "♚",
+    }
+  }
+
+}
