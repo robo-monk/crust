e98e4560ad170db2e20cfb26c507968f317ce8e2
robo-monk <rrobomonk@gmail.com>
2022-10-09 17:27:10 +0300

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod chess;
 
 // use chess::{Board};
-use chess::board::{Board};
+use chess::board::Board;
 
 fn main() {
     // let board = Board::new();
@@ -9,35 +9,35 @@ fn main() {
     let mut board = Board::from_fen(&fen);
 
     // board.get_square();
-    board.make_move("e2", "e4");
-    board.make_move("d7", "d5");
-    board.make_move("e4", "d5");
-    board.make_move("d8", "d5");
-    board.make_move("a2", "a3");
-    // // board.print();
+    // board.make_move("e2", "e4");
+    // board.make_move("d7", "d5");
+    // board.make_move("e4", "d5");
+    // board.make_move("d8", "d5");
+    // board.make_move("a2", "a3");
+    // // // board.print();
 
-    // // dbg!(board.get_available_moves("b1"));
-    board.print_available_moves("b8");
-    board.make_move("b8", "d7");
-    // // board.print_available_moves("b1"); // knight moves
-    board.make_move("b1", "c3");
-    // // board.print_available_moves("d7"); // knight moves
+    // // // dbg!(board.get_available_moves("b1"));
+    // board.print_available_moves("b8");
+    // board.make_move("b8", "d7");
+    // // // board.print_available_moves("b1"); // knight moves
+    // board.make_move("b1", "c3");
+    // // // board.print_available_moves("d7"); // knight moves
 
-    board.make_move("g8", "f6");
-    board.make_move("d1", "e2");
-    board.print_available_moves("d5"); // queen moves
+    // board.make_move("g8", "f6");
+    // board.make_move("d1", "e2");
+    // board.print_available_moves("d5"); // queen moves
 
-    board.make_move("e7", "e6");
-    board.print_available_moves("e2"); // queen moves
-    board.make_move("e2", "e5");
+    // board.make_move("e7", "e6");
+    // board.print_available_moves("e2"); // queen moves
+    // board.make_move("e2", "e5");
     // board.print_available_moves("b7"); // pawn moves
-    board.make_move("b7", "b5");
-    board.make_move("d2", "d4");
+    // board.make_move("b7", "b5");
+    // board.make_move("d2", "d4");
     // board.print_available_moves("b5"); // pawn moves
-    board.make_move("b5", "b4");
+    // board.make_move("b5", "b4");
     // board.print_available_moves("f1"); // bishiop moves
-    board.make_move("f1", "c4");
-    board.make_move("f8", "a4");
+    // board.make_move("f1", "c4");
+    // board.make_move("f8", "a4");
     // board.print_available_moves("c4"); // bishiop moves
 
     // let moves = board.get_all_possible_moves();
@@ -47,11 +47,44 @@ fn main() {
     //     _board.push_move(m);
     // }
 
+    // println!("count is {c}");
+
+    // moves.len()
+    // for m in moves {
+    // board._make_move()
+    // }
+    // println!("total moves possible {:?} [len: {:?}]", moves, moves.len());
+    // board.make_move("b5", "b4");
+    // board.print_available_moves("b1");
+
+    // board.print_available_moves("g1")
+
+    // let a = board.get_square(&"E2".to_string());
+    // dbg!(a);
+}
+
+// #[cfg(test)]
+// mod tests {
+
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
+#[test]
+fn rules_are_right() {
+    let fen = String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    let mut board = Board::from_fen(&fen);
     fn count_deep_moves(board: &Board, depth: usize) -> usize {
-        if depth <= 0 { return 0 }
+        if depth <= 0 {
+            return 1;
+        }
 
         let moves = board.get_all_possible_moves();
-        let mut move_count = moves.len();
+        // let mut move_count = moves.len();
+        let mut move_count = 0;
 
         for m in &moves {
             let mut _board = board.clone();
@@ -63,19 +96,13 @@ fn main() {
         // moves.len()
     }
 
-    let c = count_deep_moves(&board.clone(), 4);
-    println!("count is {c}");
+    let plys_count = hashmap![1 => 20, 2 => 400, 3 => 8902, 4 => 197281, 5 => 4865609, 6 => 119060324 ];
 
-    // moves.len()
-    // for m in moves {
-        // board._make_move()
+    for (ply, expected_count) in plys_count.iter() {
+        let count = count_deep_moves(&board.clone(), *ply);
+        assert_eq!(count, *expected_count as usize, "ply {ply} should be {expected_count} but was {count}");
+    }
+    // for i in 1..7 {
     // }
-    // println!("total moves possible {:?} [len: {:?}]", moves, moves.len());
-    // board.make_move("b5", "b4");
-    // board.print_available_moves("b1");
-
-    // board.print_available_moves("g1")
-
-    // let a = board.get_square(&"E2".to_string());
-    // dbg!(a);
 }
+// }
