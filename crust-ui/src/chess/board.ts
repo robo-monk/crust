9b5aad84d7bfe3e37855c58f2e7a3c2b20950f8d
robo-monk <rrobomonk@gmail.com>
2022-10-20 21:15:16 +0200

    abstract the abstraction

--- crust-ui/src/chess/board.ts
+++ crust-ui/src/chess/board.ts
@@ -2,17 +2,28 @@ import init, { greet, parse_fen, get_squares } from "../../crust";
 
 const initFen = "r1bqkbnr/1pp2p1p/p1n5/1P1P4/P1p1p1p1/5N2/4PPPP/RNBQKB1R b KQkq - 0 1"
 
-init().then(() => {
-  // console.log("helloooooo ")
-  let board = parse_fen(initFen);
-  console.log('board is', board);
-  let squares = get_squares(board);
-  console.log('squares are', squares);
-
-  // greet("Webassambly")
-
-})
-
+await init()
 
 export class Board {
+  private s: string
+  constructor (s: string) {
+    this.s = s;
+  }
+
+  get squares() {
+    return get_squares(this.s);
+  }
+
+  static fromFen(fen: string) {
+    return new Board(parse_fen(fen))
+  }
 }
+
+  // console.log("helloooooo ")
+let board = Board.fromFen(initFen);
+console.log('board is', board);
+
+let squares = board.squares;
+console.log('squares are', squares);
+
+  // greet("Webassambly")
